1d9a27cf046affe3980c8c1b1735ee85175c0319
Bmore0fm3 <36344386+bmore0fm3@users.noreply.github.com>
2023-05-29 14:20:09 -0400

    Update challenege
    Finished challenge

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tenet
 
-![alt text](/img/TenetLogo.png "TENET LOGO")
+![alt text](/img/Tenet.png "TENET LOGO")
 
 A palindrome is a word, phrase or sequence that reads the same backwards or forward. Write a program that checks if the phrase entered is a palindrome.
--- js/site.js
+++ js/site.js
@@ -1,4 +1,69 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+//controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let reversedString = reverseString(userString);
+    let palindromeCheck = palindromeChecker(reversedString, userString);
+
+    displayString(reversedString, userString, palindromeCheck);
+
+}
+
+
+//logic functions
+function reverseString(userString){
+    let revString = [];
+
+    for (let index = userString.length - 1; index >= 0; index --){
+        revString += userString[index];
+    }
+
+    return revString
+}
+
+function palindromeChecker(revString, userString) {
+    isPalindrome = "";
+    revString = revString.toLowerCase();
+    userString = userString.toLowerCase();
+
+    if (revString == userString) {
+        isPalindrome = true;
+    } else {
+        isPalindrome = false;
+    }
+
+    return isPalindrome
+}
+
+//display function
+function displayString(reversedString, userString, palindromeCheck) {
+
+    if (palindromeCheck) {
+        //write message to page
+        document.getElementById("msg").innerHTML = `Your string reversed is: ${reversedString}`;
+
+        //Change alert color
+        document.getElementById("alert").classList.remove("alert-danger");
+        document.getElementById("alert").classList.add("alert-warning");
+
+        //show alert box
+        document.getElementById("msgHeading").innerHTML = "Yay!"
+        document.getElementById("alert").classList.remove("invisible");
+    } else {
+        //write message to page
+        document.getElementById("msg").innerHTML = `Your string is: ${userString}. The reversed string: ${reversedString}`;
+
+        //Change message header and alert box color
+        document.getElementById("msgHeading").innerHTML = "Nope!"
+
+        document.getElementById("alert").classList.remove("alert-warning");
+        document.getElementById("alert").classList.add("alert-danger");
+
+        //show alert box
+        document.getElementById("alert").classList.remove("invisible");
+
+    }
+
 }
